07c97dea6fe57c1875891e6ffa612eeffddc8b48
dokkas <31980460+dokkas@users.noreply.github.com>
2017-09-15 13:15:56 +0530

    Add files via upload

---
+++ students-array (1)/student-array/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		this.Student = setStudent();
+		this.Student = getStudent();
+		this.Student = addFirst();
+		this.Student = addLast();
+		this.Student=remove_student id;
+		this.Student=remove_student name;
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
